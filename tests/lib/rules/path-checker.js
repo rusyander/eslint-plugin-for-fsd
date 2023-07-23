126176a70a046b836e7afad8637ef107562cd53d
rusyander <rustam.urmanov.1996@mail.ru>
2023-07-23 15:38:46 +0500

    feat: add file import checker

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -11,21 +11,46 @@
 const rule = require("../../../lib/rules/path-checker"),
   RuleTester = require("eslint").RuleTester;
 
-
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+  },
+});
 ruleTester.run("path-checker", rule, {
   valid: [
-    // give me some code that won't trigger a warning
+    // {
+    //   filename:
+    //     "D:\\Work\\react-tests\\react-ts-production\\src\\features\\AuthByUserName\\model\\slice\\loginSlice.ts",
+    //   code: "import { LoginReducer } from '../AuthByUserName/model/slice/loginSlice';",
+    // },
   ],
 
   invalid: [
     {
-      code: "error path",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      filename:
+        "D:\\Work\\react-tests\\react-ts-production\\src\\features\\AuthByUserName\\model\\slice\\loginSlice.ts",
+      code: "import { LoginReducer } from '@/features/AuthByUserName/model/slice/loginSlice';",
+      errors: [
+        {
+          message: "В рамках одного слайса все пути должны быть относительными",
+        },
+      ],
+      options: [{ alias: "@" }],
     },
+    // {
+    //   filename:
+    //     "D:\\Work\\react-tests\\react-ts-production\\src\\features\\AuthByUserName\\model\\slice\\loginSlice.ts",
+    //   code: "import { LoginReducer } from 'features/AuthByUserName/model/slice/loginSlice';",
+    //   errors: [
+    //     {
+    //       message: "В рамках одного слайса все пути должны быть относительными",
+    //     },
+    //   ],
+    // },
   ],
 });
